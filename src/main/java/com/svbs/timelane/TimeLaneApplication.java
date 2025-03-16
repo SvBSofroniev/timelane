23db9c2fa3bad75067693273ea3d42e775de8349
Svetlin Sofroniev <svetlinbiserov@gmail.com>
2025-03-16 17:04:38 +0200

    Initial commit.

---
+++ src/main/java/com/svbs/timelane/TimeLaneApplication.java
@@ -0,0 +1,11 @@
+package com.svbs.timelane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLaneApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLaneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/svbs/timelane/TimeLaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svbs.timelane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
